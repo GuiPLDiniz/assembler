11c1017ae04af228c7c86b5abdc6d59979c6d0eb
Guilherme Pessoa Lima Diniz <glimadiniz2@gmail.com>
2026-06-28 10:47:52 -0300

    feat(symbol-table): implementa primeira passagem para labels

--- src/main.py
+++ src/main.py
@@ -1,14 +1,31 @@
+from pathlib import Path
+
+from parser import Parser
 from symbol_table import SymbolTable
 
-table = SymbolTable()
 
-print(table.get_address("SP"))
-print(table.get_address("LCL"))
-print(table.get_address("R0"))
-print(table.get_address("R15"))
-print(table.get_address("SCREEN"))
-print(table.get_address("KBD"))
+input_file = Path("tests/test.asm")
 
-table.add_entry("LOOP", 10)
-print(table.contains("LOOP"))
-print(table.get_address("LOOP"))
+symbol_table = SymbolTable()
+parser = Parser(input_file)
+
+rom_address = 0
+
+while parser.has_more_commands():
+
+    parser.advance()
+
+    command_type = parser.command_type()
+
+    if command_type == Parser.L_COMMAND:
+
+        label = parser.symbol()
+
+        if not symbol_table.contains(label):
+            symbol_table.add_entry(label, rom_address)
+
+    else:
+        rom_address += 1
+
+
+print("LOOP:", symbol_table.get_address("LOOP"))
--- tests/test.asm
+++ tests/test.asm
@@ -1,6 +1,5 @@
 @2
 D=A
-@3
-D=D+A
-@0
-M=D
+(LOOP)
+@LOOP
+0;JMP
