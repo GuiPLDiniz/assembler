81c08caaa8fe9d41b354b3b5c8c813638004d05e
Guilherme Pessoa Lima Diniz <glimadiniz2@gmail.com>
2026-06-28 10:34:06 -0300

    feat(main): integra traducao de instrucoes A e C

--- src/main.py
+++ src/main.py
@@ -1,5 +1,37 @@
+from pathlib import Path
+
+from parser import Parser
 from code import Code
 
-print(Code.c_instruction("D", "A", None))
-print(Code.c_instruction(None, "0", "JMP"))
-print(Code.c_instruction("M", "D+1", None))
+
+input_file = Path("tests/test.asm")
+output_file = input_file.with_suffix(".hack")
+
+parser = Parser(input_file)
+
+with open(output_file, "w", encoding="utf-8") as output:
+
+    while parser.has_more_commands():
+
+        parser.advance()
+        command_type = parser.command_type()
+
+        if command_type == Parser.A_COMMAND:
+
+            symbol = parser.symbol()
+
+            if symbol.isdigit():
+
+                output.write(Code.a_instruction(symbol) + "\n")
+
+        elif command_type == Parser.C_COMMAND:
+
+            output.write(
+                Code.c_instruction(
+                    parser.dest(),
+                    parser.comp(),
+                    parser.jump()
+                ) + "\n"
+            )
+
+print("Arquivo gerado:", output_file)
