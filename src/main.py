e5f76114559e4d849a9652eea746c92917b53c1f
Guilherme Pessoa Lima Diniz <glimadiniz2@gmail.com>
2026-06-28 10:30:41 -0300

    feat(code): implementa tabelas de tradução das C-instructions

--- src/main.py
+++ src/main.py
@@ -1,28 +1,5 @@
-from pathlib import Path
-
-from parser import Parser
 from code import Code
 
-
-input_file = Path("tests/test.asm")
-output_file = input_file.with_suffix(".hack")
-
-parser = Parser(input_file)
-
-with open(output_file, "w") as output:
-
-    while parser.has_more_commands():
-
-        parser.advance()
-
-        if parser.command_type() == Parser.A_COMMAND:
-
-            symbol = parser.symbol()
-
-            if symbol.isdigit():
-
-                output.write(
-                    Code.a_instruction(symbol) + "\n"
-                )
-
-print("Arquivo gerado:", output_file)
+print(Code.c_instruction("D", "A", None))
+print(Code.c_instruction(None, "0", "JMP"))
+print(Code.c_instruction("M", "D+1", None))
